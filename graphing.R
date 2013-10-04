1a0d7e53d297af565cbab3ac4d93110a16ff2749
Polina Giralt <polina.giralt@gmail.com>
2013-10-04 16:15:54 -0400

    plotting graphs with NYC housing data

--- graphing.R
+++ graphing.R
@@ -11,3 +11,15 @@ summary(heightMod)
 33.8 + .514*father.son$fheight
 housing <- read.table("http://jaredlander.com/data/housing.csv", header=TRUE, sep=",", stringsAsFactors=FALSE)
 View(housing)
+
+names(housing) <- c("Neighborhood", "Class", "Units", "YearBuilt", "SqFt", "Income", "IncomePerSqFt", "Expense", "ExpensePerSqFt", "NetIncome", "Value", "ValuePerSqFt", "Boro")
+ggplot(housing, aes(x=ValuePerSqFt)) + geom_histogram()
+ggplot(housing, aes(x=ValuePerSqFt)) + geom_histogram(aes(fill=Boro))
+ggplot(housing, aes(x=ValuePerSqFt)) + geom_histogram(aes(fill=Boro)) + facet_wrap(~Boro)
+
+housing <- housing[housing$Units < 1000, ]
+
+houseMod1 <- lm(ValuePerSqFt ~ Units + SqFt + Boro, data=housing)
+length(unique(housing$Boro))
+
+houseMod1
