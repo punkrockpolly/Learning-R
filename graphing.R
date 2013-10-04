bc346719df603dfa8aad0d264102bd3d94974a14
Polina Giralt <polina.giralt@gmail.com>
2013-10-04 16:57:32 -0400

    Modeling NYC housing data and using AIC to determine best Model

--- graphing.R
+++ graphing.R
@@ -23,3 +23,40 @@ houseMod1 <- lm(ValuePerSqFt ~ Units + SqFt + Boro, data=housing)
 length(unique(housing$Boro))
 
 houseMod1
+summary(houseMod1)
+
+require(coefplot)
+coefplot(houseMod1)
+coefplot(houseMod1, lwdOuter=1, lwdInner=2, sort="mag")
+head(model.matrix(~Boro, data=housing))
+
+houseMod2 <- lm(ValuePerSqFt ~ Units * SqFt + Boro, data=housing)
+houseMod3 <- lm(ValuePerSqFt ~ Units:SqFt + Boro, data=housing)
+houseMod4 <- lm(ValuePerSqFt ~ SqFt* Units * Income, data=housing)
+houseMod5 <- lm(ValuePerSqFt ~ Class * Boro, data=housing)
+houseMod6 <- lm(ValuePerSqFt ~ SqFt* Units * Income * Boro, data=housing)
+
+head(model.matrix(ValuePerSqFt ~ Units * SqFt, data=housing))
+# * creates new cols for multiplied values
+head(model.matrix(ValuePerSqFt ~ Units : SqFt, data=housing)) 
+# : used for interaction, just the multiplication
+head(model.matrix(ValuePerSqFt ~ SqFt * Units * Income, data=housing)) 
+# data in the matrix with additional multiplied cols
+
+head(model.matrix(ValuePerSqFt ~ I(Units^2) + SqFt, data=housing)) 
+# I() allows you to have higher order computations
+
+houseMod7 <- lm(ValuePerSqFt ~ I(Units^2) + SqFt, data=housing)
+houseMod8 <- lm(ValuePerSqFt ~ Units + SqFt, data=housing)
+
+coef(house6)
+coef(house7)
+View(model.matrix(ValuePerSqFt ~ Class * Boro, housing))
+unique(housing$Class)
+unique(housing$Boro)
+
+multiplot(houseMod1, houseMod2, houseMod3)
+
+AIC(houseMod1, houseMod2, houseMod3, houseMod4, houseMod5, houseMod6, houseMod7, houseMod8)
+# AIC is the approach to evaluate the suitability for model, lower the better
+
