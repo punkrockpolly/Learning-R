863848a109a9baaa979362554c595d2e0337bd11
Polina Giralt <polina.giralt@gmail.com>
2013-10-04 17:30:31 -0400

    clustering wine data in R

--- clustering.R
+++ clustering.R
@@ -1,2 +1,9 @@
 wine <- read.table("http://www.jaredlander.com/data/wine.csv", header=TRUE, sep=",", stringsAsFactors=FALSE)
 View(wine)
+
+set.seed(2746251)
+winek3 <- kmeans(x=wine, centers=3)
+winek3$cluster
+winek3$centers
+require(useful)
+plot(winek3, data=wine)
