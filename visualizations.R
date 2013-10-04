974e18f8a1a2fb0627c2a7da463a42ddf1e65df8
Polina Giralt <polina.giralt@gmail.com>
2013-10-04 14:41:30 -0400

    added code for some nifty visualizations using ggplot, MAGIC!

--- visualizations.R
+++ visualizations.R
@@ -14,3 +14,12 @@ ggplot(diamonds, aes(x=price)) + geom_histogram(binwidth=50)
 ?"~"
 
 ggplot(diamonds, aes(x=price)) + geom_histogram() + facet_wrap(~cut)
+ggplot(diamonds, aes(x=price)) + geom_histogram(aes(fill=cut))
+
+g <- ggplot(diamonds, aes(x=carat, y=price))
+g + geom_point() 
+g + geom_point() + facet_wrap(~cut)
+g + geom_point(aes(color=cut)) 
+g + geom_point(aes(color=table)) + scale_color_continuous(low="blue", high="red")
+g + geom_point() + facet_grid(color~clarity)
+g + geom_point(aes(color=cut)) + facet_grid(color~clarity)
