2612493032f1d3202a2319d0dedd6f088093d94f
Polina Giralt <polina.giralt@gmail.com>
2013-10-04 17:41:51 -0400

    clustering wine data in R

--- clustering.R
+++ clustering.R
@@ -7,3 +7,14 @@ winek3$cluster
 winek3$centers
 require(useful)
 plot(winek3, data=wine)
+
+# Dendrogram for measuing heiarchical clusters (euclidian distance) AKA cluster trees
+wineH <- hclust(d = dist(wine))
+dist(wine) 
+# calculates distance between rows of data
+# scale is how far apart they are
+
+plot(wineH)
+
+rect.hclust(wineH, k=3, border = "red")
+rect.hclust(wineH, k=13, border = "blue")
