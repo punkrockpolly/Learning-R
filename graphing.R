adfbb868a5b139b638f1146fefba534032a3f94d
Polina Giralt <polina.giralt@gmail.com>
2013-10-04 15:56:38 -0400

    plotting some more graphs with NYC housing data

--- graphing.R
+++ graphing.R
@@ -7,3 +7,7 @@ ggplot(father.son, aes(x=fheight, y=sheight)) + geom_point() + geom_smooth(metho
 heightMod <- lm(sheight ~ fheight, data=father.son)
 heightMod
 summary(heightMod)
+?father.son
+33.8 + .514*father.son$fheight
+housing <- read.table("http://jaredlander.com/data/housing.csv", header=TRUE, sep=",", stringsAsFactors=FALSE)
+View(housing)
